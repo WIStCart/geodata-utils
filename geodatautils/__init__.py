a38972073fe7d8ae5da9bbaa2ed0e4d62d8aad03
Hayden Elza <HaydenElza@users.noreply.github.com>
2023-10-10 15:31:39 -0500

    Update logging to have a default indent
    Update logging to have a default indent so that when one is not specified, the program will continue to work. This is important since some imported modules use logging but won't have the custom indent property.

--- geodatautils/__init__.py
+++ geodatautils/__init__.py
@@ -11,14 +11,18 @@ __author__ = "Hayden Elza"
 __license__ = "GPL-3"
 
 
-# Config
+# Read in Config
 dirname = os.path.dirname(__file__)
 with open(os.path.join(dirname, '../config/config.yml'), 'r') as f:
-
-    # Read in config
     config = yaml.safe_load(f.read())
 
-    # Config logging
-    logging.config.dictConfig(config['log'])
+# Config logging
+logging.config.dictConfig(config['log'])
 
-    
+# Set default indent value so that it does not need to be specified
+class CustomFilter(logging.Filter):
+    def filter(self, record):
+        if not hasattr(record, 'indent'):
+            record.indent = " "
+        return True
+logging.getLogger().addFilter(CustomFilter())
