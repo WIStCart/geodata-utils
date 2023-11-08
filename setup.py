7e504ca5468c9a619d07c127ee44384b5f4c998d
Hayden Elza <HaydenElza@users.noreply.github.com>
2023-11-08 17:39:21 -0600

    Added pyproject.toml

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='geodatautils',
-    version='0.1',
-    description='Utilities for managing the GeoData@Wisconsin GeoBlacklight instance.',
-    author='Hayden Elza',
-    license='GPLv3',
-    packages=find_packages(where='geodatautils'),
-    package_data={
-        "geodatautils.config": ["config.yml"],
-        "geodatautils.config.schemas": ["*.json"],
-    },
-    install_requires=['pyyaml','requests','jsonschema'],
-    entry_points={
-        'console_scripts': [
-            'update_solr=geodatautils.interfaces:update_solr',
-        ]
-    }
-)
+setup()
