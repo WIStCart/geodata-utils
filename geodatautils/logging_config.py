b7cd65d709cf7cc7748fdc531603b25509af983c
Hayden Elza <HaydenElza@users.noreply.github.com>
2024-01-08 19:20:49 -0600

    Added ability for `empty_missing()` to take in str or list, Added `error-checks` to config, Added label to logging formatter

--- geodatautils/logging_config.py
+++ geodatautils/logging_config.py
@@ -7,12 +7,14 @@ import logging
 
 
 class DefaultIndent(logging.Filter, ):
-    """Logging filter to allow the indent property to not be specified."""
+    """Logging filter to allow optional properties to not be specified."""
 
     def filter(self, record):
-        """Set default indent value so that it does not need to be specified"""
+        """Set default values so that they do not need to be specified"""
         if not hasattr(record, 'indent'):
             record.indent = " "
+        if not hasattr(record, 'label'):
+            record.label = ""
         return True
   
     
@@ -22,6 +24,10 @@ class LogFormat:
     def indent(level, tree=False):
         """Indent with tabs with the option of a tree prefix."""
         return "\t"*level*2 + ("└── " if tree else "")
+
+    def label(label:str):
+        """Add label to log entry. E.g., '(some-function)'"""
+        return "(" + label + ") "
     
     def spaces(n):
         """Generate a specified number of spaces."""
--- geodatautils/schema.py
+++ geodatautils/schema.py
@@ -8,6 +8,7 @@ geoblacklight.
 import logging
 import re
 from importlib.resources import files
+from typing import Union
 
 from jsonschema import validators
 from jsonschema.exceptions import SchemaError
@@ -18,16 +19,24 @@ from .logging_config import LogFormat
 from .solr import Solr
 
 
-def empty_missing(data:dict, property:str) -> bool:
-    try:
-        if data[property] == "": 
-            logging.error("'{}' is empty".format(property), extra={'indent': LogFormat.indent(2, True)})
+def empty_missing(data:dict, fields:Union[str, list], label:str=None) -> bool:
+    
+    # If fields is a string, recast to list
+    if type(fields) == str:
+        fields = [fields]
+    
+    # Check each field to see if it is empty or missing
+    for field in fields:
+        try:
+            if data[field] == "": 
+                logging.error("'{}' is empty".format(field), extra={'indent': LogFormat.indent(2, True), 'label': LogFormat.label(label)})
+                return True
+        
+        except KeyError:
+            logging.error("Required field '{}' was not found.".format(field), extra={'indent': LogFormat.indent(2, True), 'label': LogFormat.label(label)})
             return True
     
-    except KeyError:
-        logging.error("Required property '{}' was not found.".format(property), extra={'indent': LogFormat.indent(2, True)})
-        return True
-    
+    # If no fields are empty or missing
     return False
 
 
@@ -48,45 +57,56 @@ def error_check(data:dict, solr:Solr) -> bool:
     errors = False
 
     # Fields are not null
-    fields = ['dc_title_s', 'dc_identifier_s', 'layer_slug_s', 'solr_geom', 'dct_provenance_s', 'dc_rights_s', 'geoblacklight_version', 'dc_creator_sm', 'dc_description_s', 'dct_references_s', 'dct_temporal_sm', 'solr_year_i', 'layer_modified_dt']
-    for field in fields:
-        if empty_missing(data, field):
-            errors = True
+    if 'properties-not-null' in config['error-checks'] and type(config['error-checks']['properties-not-null']) == list:
+        fields = config['error-checks']['properties-not-null']
+        for field in fields:
+            if empty_missing(data, field, 'properties-not-null'):
+                errors = True
 
     # Check that dc_identifier_s and layer_slug_s match
-    if data['dc_identifier_s'] != data['layer_slug_s']:
-        logging.error("'dc_identifier_s' and 'layer_slug_s' do not match", extra={'indent': LogFormat.indent(2, True)})
-        logging.debug("'{}', '{}'".format(data['dc_identifier_s'], data['layer_slug_s']), extra={'indent': LogFormat.indent(3)})
-        errors = True
+    error_check_name = 'identifier-layer-slug-match'
+    if config['error-checks'][error_check_name] and not empty_missing(data, ['dc_identifier_s', 'layer_slug_s'], error_check_name):
+        if data['dc_identifier_s'] != data['layer_slug_s']:
+            logging.error("'dc_identifier_s' and 'layer_slug_s' do not match", extra={'indent': LogFormat.indent(2, True), 'label': LogFormat.label(error_check_name)})
+            logging.debug("'{}', '{}'".format(data['dc_identifier_s'], data['layer_slug_s']), extra={'indent': LogFormat.indent(3)})
+            errors = True
 
     # Check that dct_temporal_sm contains solr_year_i
-    if not any(str(data['solr_year_i']) in item for item in data['dct_temporal_sm']):
-        logging.error("'dct_temporal_sm' does not contain 'solr_year_i'", extra={'indent': LogFormat.indent(2, True)})
-        logging.debug("'{}', '{}'".format(data['dct_temporal_sm'], data['solr_year_i']), extra={'indent': LogFormat.indent(3)})
-        errors = True
+    error_check_name = 'temportal-contains-solr-year'
+    if config['error-checks'][error_check_name] and not empty_missing(data, ['solr_year_i', 'dct_temporal_sm'], error_check_name):
+        if not any(str(data['solr_year_i']) in item for item in data['dct_temporal_sm']):
+            logging.error("'dct_temporal_sm' does not contain 'solr_year_i'", extra={'indent': LogFormat.indent(2, True), 'label': LogFormat.label(error_check_name)})
+            logging.debug("'{}', '{}'".format(data['dct_temporal_sm'], data['solr_year_i']), extra={'indent': LogFormat.indent(3)})
+            errors = True
 
     # Check that dc_title_s contains solr_year_i 
-    if str(data['solr_year_i']) not in data['dc_title_s']:
-        logging.error("'dc_title_s' does not contain 'solr_year_i'", extra={'indent': LogFormat.indent(2, True)})
-        logging.debug("'{}', '{}'".format(data['dc_title_s'], data['solr_year_i']), extra={'indent': LogFormat.indent(3)})
-        errors = True
+    error_check_name = 'title-contains-solr-year'
+    if config['error-checks'][error_check_name] and not empty_missing(data, ['solr_year_i', 'dc_title_s'], error_check_name):
+        if str(data['solr_year_i']) not in data['dc_title_s']:
+            logging.error("'dc_title_s' does not contain 'solr_year_i'", extra={'indent': LogFormat.indent(2, True), 'label': LogFormat.label(error_check_name)})
+            logging.debug("'{}', '{}'".format(data['dc_title_s'], data['solr_year_i']), extra={'indent': LogFormat.indent(3)})
+            errors = True
 
-    # Check that solr_year_i is in dct_references_s['http://schema.org/downloadUrl\']
-    regex = '(?<=downloadUrl":").+?([^\/]+?)(?=")'
-    matches = re.findall(regex, data['dct_references_s'])
+    # Check that dct_references_s['http://schema.org/downloadUrl\'] contains solr_year_i
+    error_check_name = 'references-contains-solr-year'
+    if config['error-checks'][error_check_name] and not empty_missing(data, ['solr_year_i', 'dct_references_s'], error_check_name):
+        regex = '(?<=downloadUrl":").+?([^\/]+?)(?=")'
+        matches = re.findall(regex, data['dct_references_s'])
 
-    if not any(str(data['solr_year_i']) in match for match in matches):
-        logging.error("""'dct_references_s["http://schema.org/downloadUrl"]' does not contain 'solr_year_i'""", extra={'indent': LogFormat.indent(2, True)})
-        logging.debug("{}, '{}'".format(matches, data['solr_year_i']), extra={'indent': LogFormat.indent(3)})
-        errors = True
+        if not any(str(data['solr_year_i']) in match for match in matches):
+            logging.error("""'dct_references_s["http://schema.org/downloadUrl"]' does not contain 'solr_year_i'""", extra={'indent': LogFormat.indent(2, True), 'label': LogFormat.label(error_check_name)})
+            logging.debug("{}, '{}'".format(matches, data['solr_year_i']), extra={'indent': LogFormat.indent(3)})
+            errors = True
 
     # Check for existing UID (`dc_identifier_s`) in current Solr index
-    raw_response = solr.select(q=data['dc_identifier_s'])
-    records_found = raw_response['response']['numFound']
-    if records_found > 0:
-        logging.error("""'dc_identifier_s' already exists in the Solr index.""", extra={'indent': LogFormat.indent(2, True)})
-        logging.debug("{} records found for '{}'".format(records_found, data['dc_identifier_s']), extra={'indent': LogFormat.indent(3)})
-        errors = True
+    error_check_name = 'existing-uid'
+    if config['error-checks'][error_check_name] and not empty_missing(data, ['dc_identifier_s'], error_check_name):
+        raw_response = solr.select(q=data['dc_identifier_s'])
+        records_found = raw_response['response']['numFound']
+        if records_found > 0:
+            logging.error("""'dc_identifier_s' already exists in the Solr index.""", extra={'indent': LogFormat.indent(2, True), 'label': LogFormat.label(error_check_name)})
+            logging.debug("{} records found for '{}'".format(records_found, data['dc_identifier_s']), extra={'indent': LogFormat.indent(3)})
+            errors = True
 
     return errors
 
@@ -117,7 +137,7 @@ def validate(data:dict, schema_name:str) -> bool:
         # Log errors
         for error in v.iter_errors(data):
             for suberror in sorted(error.context, key=lambda e: e.schema_path)[:-1]:
-                logging.error("Schema Validation Error: {}: {}".format(":".join(map(str,list(suberror.schema_path))), suberror.message), extra={'indent': LogFormat.indent(2, True)})
+                logging.error("{}: {}".format(":".join(map(str,list(suberror.schema_path))), suberror.message), extra={'indent': LogFormat.indent(2, True), 'label': LogFormat.label("schema-validation")})
 
         return False
     
