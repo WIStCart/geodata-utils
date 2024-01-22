2587c28292f9a33894483bd7c1e6ff18bf93fcd9
Hayden Elza <HaydenElza@users.noreply.github.com>
2024-01-22 14:50:31 -0600

    Added logic to handle if config file is not yet initiated

--- geodatautils/__init__.py
+++ geodatautils/__init__.py
@@ -9,6 +9,7 @@ __author__ = "Hayden Elza"
 __license__ = "GPL-3"
 
 
+import os
 import logging
 import logging.config
 from importlib.resources import files
@@ -16,8 +17,15 @@ from importlib.resources import files
 import yaml
 
 
+# Set config path
+config_path = files('geodatautils.config').joinpath('config.yml')
+
+# Confirm config exists, if not initiate config
+if not os.path.exists(config_path):
+    os.system("gu_config -i")
+
 # Read in Config
-with open(files('geodatautils.config').joinpath('config.yml'), 'r') as f:
+with open(config_path, 'r') as f:
     config = yaml.safe_load(f.read())
 
 # Config logging
